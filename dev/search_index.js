53bc6da2cd0ef4ca7fa5c990874c8203aec555b5
Documenter.jl <documenter@juliadocs.github.io>
2023-04-29 18:29:24 +0000

    build based on bc5b40e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Table3Landais","category":"page"},{"location":"#Table3Landais","page":"Home","title":"Table3Landais","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Table3Landais.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Table3Landais]","category":"page"}]
+}
